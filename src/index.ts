85621120b3ad8303328085a916668c7821fe51b2
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2021-05-02 20:19:59 -0600

    Export the AggregateError class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import "source-map-support/register";
 import callBind from "call-bind";
 import define from "define-properties";
 
+import AggregateError from "es-aggregate-error";
 import requirePromise from "./requirePromise";
 import implementation from "./implementation";
 import getPolyfill from "./polyfill";
@@ -9,13 +11,25 @@ import shim from "./shim";
 requirePromise();
 const bound = callBind(getPolyfill());
 
+interface Rebindable {
+  AggregateError: typeof AggregateError;
+  getPolyfill: typeof getPolyfill;
+  implementation: typeof implementation;
+  shim: typeof shim;
+  <T>(this: unknown, iterable: Iterable<Promise<T>>): Promise<T>;
+}
+
 // eslint-disable-next-line func-name-matching
-const rebindable = function any<T>(this: unknown, iterable: Iterable<Promise<T>>): Promise<T> {
+const rebindable: Rebindable = function any<T>(
+  this: unknown,
+  iterable: Iterable<Promise<T>>
+): Promise<T> {
   // eslint-disable-next-line no-invalid-this
   return bound(typeof this === "undefined" ? Promise : this, iterable);
-};
+} as Rebindable;
 
 define(rebindable, {
+  AggregateError: AggregateError,
   getPolyfill: getPolyfill,
   implementation: implementation,
   shim: shim
